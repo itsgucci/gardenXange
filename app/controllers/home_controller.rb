227da458d1c22b9b183c7da68213153130088344
Lisa Sheridan <owner@codevelvet.com>
2012-05-06 13:59:11 -0700

    Fixed bug with stale session id not returning user causing exception

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
   # GET /show
   def show
-    @user = User.new
     @user_count = User.count
-    if user_authenticated?
+    if user_authenticated? and User.find_by_id(session[:user_id])
       @user = User.find_by_id(session[:user_id])
       respond_to do |format|
         format.html { render :notice => "Welcome back #{@user.username}" } # show.html.erb
       end
     else
+      @user = User.new
       respond_to do |format|
         format.html { render :action => :welcome }
       end
